bcbb938d5a2fb0688d701f3ab053f36951b83dbe
Aleksey <antokhin.aleksey@gmail.com>
2019-05-06 05:48:45 +0300

    Initial Commit

---
+++ Calculator_3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator_3
+//
+//  Created by Aleksey3a on 06.05.19.
+//  Copyright © 2019 Antokhin Aleksey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
